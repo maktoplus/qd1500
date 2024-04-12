511fea71e5af5d6290c8074f4f4e54a5da614624
Lcc <youtao531@163.com>
2024-04-12 11:21:51 +0800

    first commit

--- APP/Common/Conf/config.php
+++ APP/Common/Conf/config.php
@@ -4,7 +4,7 @@ return array(
 	'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => 'riskctl.cfjq6sky0wee.ap-south-1.rds.amazonaws.com', // 服务器地址
     'DB_NAME'   => 'qd1500_jglgs_com', // 数据库名
-    'DB_USER'   => 'root', // 用户名
+    'DB_USER'   => 'php', // 用户名
     'DB_PWD'    => '12345678', // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'ysk_', // 数据库表前缀
